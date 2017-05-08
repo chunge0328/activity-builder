b987c391cf0f1681bd6dff2c3e9b0a2a0f41c0ba
吴燕波 <wuyanbo@meizu.com>
2017-05-08 19:00:04 +0800

    添加文件

--- src/app/activity/App.vue
+++ src/app/activity/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="app">
-<LazyImage/><Flip/><Flip/><LazyImage/><Disk/><GameBar/><Flip/><Disk/><GameBar/><Disk/><GameBar/><LazyImage/><Flip/><XText/>
+<Disk/><Flip/><LazyImage/><Flip/><LazyImage/><Flip/><GameBar/><Flip/><Disk/><GameBar/><Disk/><GameBar/><LazyImage/><Flip/><XText/>
   </div>
 </template>
 <script>
   import vue from 'vue';
   import 'quill/dist/quill.core.css';
-  import LazyImage from "business/LazyImage/index.vue";
-  import Flip from "business/Flip/index.vue";
   import Disk from "business/Disk/index.vue";
+  import Flip from "business/Flip/index.vue";
+  import LazyImage from "business/LazyImage/index.vue";
   import GameBar from "business/GameBar/index.vue";
   import XText from "business/XText/index.vue";
   vue.use({
     install(vue) {
-      vue.prototype.__STORE__ = ({ "0.0": { "propsData": {}, "staticStyle": {} }, "0.0.4": { "propsData": {}, "staticStyle": {} }, "0.0.4.0": { "propsData": { "txt": "按钮sdfsdfdsafd" }, "staticStyle": { "transform": "translate(167px,-9px)", "webkitTransform": "translate(167px,-9px)" } }, "0.0.11": { "propsData": {}, "staticStyle": {} }, "0.0.10": { "propsData": {}, "staticStyle": {} }, "0.0.9": { "propsData": {}, "staticStyle": {} }, "0.0.8": { "propsData": {}, "staticStyle": {} }, "0.0.7": { "propsData": {}, "staticStyle": {} }, "0.0.6": { "propsData": {}, "staticStyle": {} }, "0.0.12": { "propsData": {}, "staticStyle": {} }, "0.0.13": { "propsData": {}, "staticStyle": {} }, "0.0.5": { "propsData": {}, "staticStyle": {} } });
+      vue.prototype.__STORE__ = ({ "0.0": { "propsData": {}, "staticStyle": {} }, "0.0.9": { "propsData": {}, "staticStyle": {} }, "0.0.8": { "propsData": {}, "staticStyle": {} }, "0.0.6": { "propsData": {}, "staticStyle": {} }, "0.0.5": { "propsData": {}, "staticStyle": {} }, "0.0.0": { "propsData": {}, "staticStyle": {} }, "0.0.0.0": { "propsData": { "txt": "按钮sdfsdfdsafd", "bgImg": { "url": require("assets/images/HJNG0TpkZ.jpg"), "width": 1536, "height": 764 }, "shape": "OVAL" }, "staticStyle": { "transform": "translate(62px,-3px)", "webkitTransform": "translate(62px,-3px)" } }, "0.0.14": { "propsData": {}, "staticStyle": {} }, "0.0.13": { "propsData": {}, "staticStyle": {} }, "0.0.12": { "propsData": {}, "staticStyle": {} }, "0.0.11": { "propsData": {}, "staticStyle": {} }, "0.0.10": { "propsData": {}, "staticStyle": {} }, "0.0.2": { "propsData": {}, "staticStyle": {} }, "0.0.1": { "propsData": {}, "staticStyle": {} } });
     }
   });
   export default {
@@ -21,7 +21,7 @@
       return {};
     }, 
     components: {
-      LazyImage,Flip,Disk,GameBar,XText
+      Disk,Flip,LazyImage,GameBar,XText
     }
   }
 </script>
