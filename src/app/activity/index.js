233a05eab5423cbe83f41ffd0ae6a833185232c3
吴燕波 <wuyanbo@meizu.com>
2017-05-11 21:11:58 +0800

    添加功能

--- src/app/activity/index.js
+++ src/app/activity/index.js
@@ -1,8 +1,9 @@
 import Vue from 'vue';
 import App from './App.vue';
 import Mixin from '../components/base/Mixin';
-import Motions from '../components/baseMotions';
-Vue.use(Motions);
+import Aux from '../components/base/Aux';
+import  '../components/common/defines';
+Vue.use(Aux);
 Vue.mixin(Mixin);
 new Vue({
   el: '#app',
